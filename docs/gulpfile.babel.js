1d18148c30c212884bb99cda7aaf634b9ddf7b93
kojiyamauchi <all.the.small.things@me.com>
2017-12-18 16:43:46 +0900

    update gulpfile.babel.js

--- docs/gulpfile.babel.js
+++ docs/gulpfile.babel.js
@@ -36,9 +36,7 @@ import sftp from 'gulp-sftp' // sftp plugin.
 import browserSync from 'browser-sync' // browserSync.
 // Setting.
 const autoprefixerSet = ['last 2 version', 'ie >= 10', 'iOS >= 8', 'Android >= 4.4'] // setting of autoprefixer.
-const postCssPlugIn = [autoprefixer({
-  browsers: autoprefixerSet
-}), flexbug] // PostCSS plugin.
+const postCssPlugIn = [autoprefixer({ browsers: autoprefixerSet }), flexbug] // PostCSS plugin.
 const addImgDir = ('addImages/*') // added image fold,
 const dstImgDir = ('images/') // compression image fold,
 const upLoadFileWrite = (['*.html', 'css/app.min.css', 'js/core.min.js', 'images/*']) // upload file.
